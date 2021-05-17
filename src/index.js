8bb5e34db14c250e35f15fbc3bb60eecf76e73a0
gaowenbin <lotosv2010@163.com>
2021-05-17 19:46:30 +0800

    feat(嵌套路由): 实现嵌套路由

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route, Switch, Redirect, Link} from './react-router-dom'
 
@@ -9,9 +9,48 @@ const Home = () => {
   )
 }
 
+const UserAdd = () => {
+  return (
+    <div> UserAdd </div>
+  )
+}
+
+const UserList = () => {
+  return (
+    <div>
+      <Link to={{pathname: `/user/detail/1001`, state: {id: '1001', username: 'test'}}}>userList</Link>
+    </div>
+  )
+}
+
+const UserDetail = (props) => {
+  console.log(props)
+  const user = props.location.state || {}
+  const [state, setState] = useState({})
+  const getInfo = () => {
+    setState(user)
+  }
+  useEffect(() => {
+    getInfo()
+  })
+  return (
+    <div> id: {state.id}, username: {state.username} </div>
+  )
+}
+
 const User = () => {
   return (
-    <div> user </div>
+    <div>
+      <ul>
+        <li><Link to="/user/list">用户列表</Link></li>
+        <li><Link to="/user/add">添加用户</Link></li>
+      </ul>
+      <div>
+        <Route path="/user/add" component={UserAdd} />
+        <Route path="/user/list" component={UserList} />
+        <Route path="/user/detail/:id" component={UserDetail} />
+      </div>
+    </div>
   )
 }
 
@@ -24,8 +63,8 @@ function App() {
           <li><Link to='/user'>user</Link></li>
         </ul>
         <Switch>
-          <Route path='/user' component={User} />
-          <Route path='/home' component={Home} />
+          <Route path='/user' exact={false} component={User} />
+          <Route path='/home' exact={true} component={Home} />
           <Redirect from='/' to='/home' />
         </Switch>
       </div>
